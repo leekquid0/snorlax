87881945e8656dc815f618825fb1148610118412
superskibidi <rookiecookierc@gmail.com>
2025-10-10 14:56:38 +0000

    add log

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', async event => {
 function isUrl(val = "") {
   if (
     /^http(s?):\/\//.test(val) ||
-    (val.includes(".") && val.substr(0, 1) !== " ")
+    (val.includes(".") && val.substring(0, 1) !== " ")
   )
     return true;
   return false;
